9624e15439549e9ccc0342c664bd1fbbdadbe3e5
Sarunk21 <kautsarmkp@gmail.com>
2025-05-13 22:08:19 +0700

    Menambahkan halaman dashboard admin dengan komponen sidebar dan daftar navigasi. Memperbarui gaya CSS untuk tombol dan elemen sidebar.

---
+++ resources/views/components/molecules/list.blade.php
@@ -0,0 +1,6 @@
+<!-- Nav Item - Dashboard -->
+<li class="nav-item">
+    <a class="nav-link" href="{{ $list['url'] }}">
+        <p class="{{ $list['active'] ? 'btn-dashboard-dark' : 'btn-dashboard-primary' }}">{{ $list['title'] }}</p>
+    </a>
+</li>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,58 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('components.templates.master-layout');
+    $lists = [
+        [
+            'title' => 'Dashboard',
+            'url' => '/dashboard',
+            'active' => true
+        ],
+        [
+            'title' => 'Skema',
+            'url' => '/skema',
+            'active' => false
+        ],
+        [
+            'title' => 'TUK',
+            'url' => '/tuk',
+            'active' => false
+        ],
+        [
+            'title' => 'Jadwal',
+            'url' => '/jadwal',
+            'active' => false
+        ],
+        [
+            'title' => 'User',
+            'url' => '/user',
+            'active' => false
+        ],
+        [
+            'title' => 'Pembayaran Pendaftaran',
+            'url' => '/pembayaran-pendaftaran',
+            'active' => false
+        ],
+        [
+            'title' => 'Pembayaran Asesor',
+            'url' => '/pembayaran-asesor',
+            'active' => false
+        ],
+        [
+            'title' => 'Report',
+            'url' => '/report',
+            'active' => false
+        ],
+        [
+            'title' => 'APL 2',
+            'url' => '/apl-2',
+            'active' => false
+        ],
+        [
+            'title' => 'Profile',
+            'url' => '/profile',
+            'active' => false
+        ]
+    ];
+
+    return view('components.pages.dashboard-admin', compact('lists'));
 });
