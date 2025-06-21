9fe7696033d41991fffe1255db5ac7a6937f258c
pizza40 <ravi.attar.shahrulsyah@gmail.com>
2025-06-21 19:35:53 +0700

    feat: integrate asesi upload sertifikat bertanda tangan

--- app/Models/Sertif.php
+++ app/Models/Sertif.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\SoftDeletes;
+// use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Sertif extends Model
 {
-    use HasFactory, SoftDeletes;
+    // use HasFactory, SoftDeletes;
 
     protected $table = 'sertif';
     protected $fillable = ['user_id', 'skema_id', 'pendaftaran_id', 'sertifikat', 'status'];
